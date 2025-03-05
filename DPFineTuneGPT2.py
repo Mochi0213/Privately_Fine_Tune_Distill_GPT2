a14a7f3ad679747ba2622ef46fa304621ebe71be
Mochi0213 <114381362+Mochi0213@users.noreply.github.com>
2025-03-05 12:16:23 +0800

    Recode the DPFineTune with private_transformers

--- DPFineTuneGPT2.py
+++ DPFineTuneGPT2.py
@@ -1,9 +1,12 @@
-import math
 import torch
-from torch.optim import Adam
 from transformers import GPT2Tokenizer, GPT2LMHeadModel, TrainingArguments, Trainer, DataCollatorForLanguageModeling
 from datasets import load_dataset
-from utils.dp_optimizer import make_optimizer_class
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from private_transformers import PrivacyEngine
+from torch import nn
+import torch.nn.functional as F
+
 
 dataset = load_dataset("text", data_files="processed_data.txt")
 train_dataset = dataset["train"]
@@ -11,53 +14,29 @@ train_dataset = dataset["train"]
 tokenizer = GPT2Tokenizer.from_pretrained("distilgpt2")
 tokenizer.pad_token = tokenizer.eos_token
 
-
 def tokenize_function(examples):
     return tokenizer(examples["text"], padding="max_length", truncation=True, max_length=256)
 
 
-train_dataset = train_dataset.map(tokenize_function, batched=True)
+train_dataset = train_dataset.map(tokenize_function,
+                                  batched=True,
+                                  remove_columns=["text"],)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = GPT2LMHeadModel.from_pretrained("distilgpt2").to(device)
+model = GPT2LMHeadModel.from_pretrained("distilgpt2")
 
 training_args = TrainingArguments(
     output_dir="./model",
     evaluation_strategy="no",
     save_strategy="epoch",
-    num_train_epochs=200,
+    num_train_epochs=100,
     per_device_train_batch_size=10,
-    warmup_steps=100,
+    warmup_steps=1000,
     weight_decay=0.01,
     logging_dir="./logs",
     logging_steps=10,
-    save_total_limit=2,
-    learning_rate=1e-4,
-    disable_tqdm=False,
-)
-
-
-def compute_epsilon(steps, batch_size, dataset_size, sigma, delta=1e-2):
-    sampling_probability = batch_size / dataset_size
-    orders = [1 + x / 10.0 for x in range(1, 100)] + list(range(11, 64)) + [128, 256, 512]
-
-    def compute_rdp(sampling_probability, sigma, steps, orders):
-        return [steps * sampling_probability ** 2 / (2 * sigma ** 2) for order in orders]
-
-    rdp = compute_rdp(sampling_probability, sigma, steps, orders)
-    epsilon = min([rdp[i] / (orders[i] - 1) for i in range(len(orders))])
-
-    return epsilon
-
-
-optimizer = make_optimizer_class(Adam)
-optimizer = optimizer(
-    l2_norm_clip = 0.1,
-    noise_multiplier=1.0,
-    minibatch_size=training_args.per_device_train_batch_size,
-    microbatch_size=1,
-    params = model.parameters(),
-    lr = training_args.learning_rate
+    save_total_limit=10,
+    learning_rate=3e-5,
+    disable_tqdm=False
 )
 
 data_collator = DataCollatorForLanguageModeling(
@@ -65,23 +44,39 @@ data_collator = DataCollatorForLanguageModeling(
     mlm=False
 )
 
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=train_dataset,
-    tokenizer=tokenizer,
-    data_collator=data_collator,
-    optimizers=(optimizer, None)
+train_dataloader = DataLoader(train_dataset, batch_size=training_args.per_device_train_batch_size,
+                              collate_fn=data_collator)
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=training_args.learning_rate)
+
+privacy_engine = PrivacyEngine(
+    model,
+    batch_size=training_args.per_device_train_batch_size,
+    sample_size=len(train_dataset),
+    epochs=training_args.num_train_epochs,
+    max_grad_norm=0.1,
+    target_epsilon=1,
 )
+privacy_engine.attach(optimizer)
+criterion = nn.CrossEntropyLoss()
 
+for epoch in range(training_args.num_train_epochs):
+    model.train()
+    total_loss = 0
+    for batch in tqdm(train_dataloader, desc=f"Epoch {epoch + 1}/{training_args.num_train_epochs}"):
+        optimizer.zero_grad()
+        outputs = model(**batch)
 
-trainer.train()
+        labels = batch['input_ids'][:, 1:, ]
+        logits = outputs.logits[:, :-1, :].permute(0, 2, 1)
 
-steps = math.ceil(len(train_dataset) / training_args.per_device_train_batch_size) * training_args.num_train_epochs
-epsilon = compute_epsilon(steps, training_args.per_device_train_batch_size, len(train_dataset), optimizer.noise_multiplier)
-print(f"Final (ε, δ)-DP = ({epsilon:.2f}, 1e-2)")
+        loss = F.cross_entropy(logits, labels, reduction="none").mean(dim=1)
+        optimizer.step(loss=loss)
+        total_loss += loss.mean().item()
 
-model.save_pretrained("fine_tuned_gpt2_dp")
-tokenizer.save_pretrained("fine_tuned_gpt2_dp")
+    print(f"Epoch {epoch + 1} Loss: {total_loss / len(train_dataloader)}")
 
-print("Train Completed, Fine Tuned Model parameters have been stored in fine_tuned_gpt2_dp")
+model.save_pretrained("fine_tuned_gpt2")
+tokenizer.save_pretrained("fine_tuned_gpt2")
+
+print("Train Completed, Fine Tuned Model parameters have been stored in fine_tuned_gpt2")
